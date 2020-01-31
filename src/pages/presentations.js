8bf64ed677684f4af76eeb547fad5e8a16cc6e7b
Alessandro Barros <ale_alessandro_ale@hotmail.com>
2020-01-30 22:51:54 -0300

    list of presentations created

--- src/pages/presentations.js
+++ src/pages/presentations.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import dataJson from "../../public/data.json"
-
-const data = JSON.parse(dataJson);
-alert(data);
-console.log(data);
+import dataJson from "../json/data"
 
 const Presentations = () => (
     <div>
       <h1>Apresentações</h1>
-      <h3>.</h3>
 
+      {
+        dataJson["presentation"].map(presentation => (
+          <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+            <h2>{ presentation.title }</h2>
+            <img src={ presentation.image } />
+            <h3>{ presentation.speaker }</h3>
+          </div>
+        ))
+      }
 
     </div>
 )
