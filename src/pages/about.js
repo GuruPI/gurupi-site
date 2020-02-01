85701947b9a566d910cce96493722400aecac1c9
Renata Marques <Renata>
2020-02-01 18:16:28 -0300

    photo slider added in home section

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,20 +1,17 @@
 import React from "react"
-import Layout from "../components/layout"
 
 const About = () => (
   <div>
     <hr/>
     <h1><a name="about">Sobre</a></h1>
     <p>GuruPI significa Grupo de Usuários de Ruby do Piauí.</p>
-    <h3>Objetivos do Grupo</h3>
-    <p>Divulgar e promover a linguagem de programação Ruby no estado do Piauí 
+    <p>O objetivos do rupo é divulgar e promover a linguagem de programação Ruby no estado do Piauí 
       através de eventos como palestras, discussões, apresentações de projetos e afins.
       Reuniões presenciais entre os membros do grupo para que estes possam discutir, 
       ensinar e aprender sobre a linguagem Ruby e aspectos relacionados a programação e 
       desenvolvimento de software. Colaborar com a comunidade Ruby nacional com a criação 
       e tradução de conteúdos em língua portuguesa. Colaborar com a comunidade Ruby global 
       com a criação e manutenção de projetos de código fonte aberto.</p>
-    <h3>Composição</h3>
     <p>Podem participar do grupo todos aqueles que desejarem, sendo vetada a discriminação 
       dentro do mesmo, seja racial, cultural, religiosa ou sexual de acordo com a constituição 
       brasileira. Por pessoas que estejam interessadas em ensinar, aprender e evoluir no 
--- src/pages/presentations.js
+++ src/pages/presentations.js
@@ -1,13 +1,12 @@
 import React from "react"
-import Layout from "../components/layout"
 import dataJson from "../json/data"
 
 const Presentations = () => (
   <div>
     <h1><a name="presentations"> Palestras </a></h1>
     {
-      dataJson.presentations.map(presentation => (
-        <div className="card">
+      dataJson.presentations.map((presentation, index) => (
+        <div className="card" key={ index }>
           <h4 className="overlay">{ presentation.title }</h4>
           <img src={ presentation.image } className="img-shrunked"/>
           <h3>{ presentation.speaker }</h3>
