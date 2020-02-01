bf34b6fae8a8c770fca99240f3080d25bc72d9d6
Renata Marques <Renata>
2020-02-01 00:06:44 -0300

    home section added

---
+++ src/pages/home.js
@@ -0,0 +1,12 @@
+import React from "react"
+import icon from "../images/gurupi-icon.png"
+
+const Home = () => (
+  <div>
+    <a name="home">
+      <img src={ icon } />
+    </a>
+  </div>
+)
+
+export default Home;
