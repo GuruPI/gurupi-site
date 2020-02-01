2641e5cf22e2aaa2a21086062c90cd5228a9d47a
Renata Marques <Renata>
2020-01-31 22:42:52 -0300

    removing SEO

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,10 +1,7 @@
 import React from "react"
-import SEO from "../components/seo"
-import { Link } from "gatsby"
 
 const Menu = () => (
   <div>
-    <SEO title="Home" />
     <ul>
       <li><a href="#home">Home</a></li>
       <li><a href="#about">Sobre</a></li>
