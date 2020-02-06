ca23bdec8694691c79d058d16128602710e8a951
Renata Marques <renata.marques@codeminer42.com>
2020-02-06 18:47:20 -0300

    renewing home page

--- src/pages/events.js
+++ src/pages/events.js
@@ -3,9 +3,9 @@ import React from "react"
 const Events = () => (
   <div>
     <h1><a name="events">Eventos</a></h1>
-    <h4>O próximo encontro GuruPI vai acontecer 
-    dia 15 de fevereiro, no *local a definir*, 
-    das 8h às 12h</h4>
+    <h4>O próximo encontro GuruPI vai acontecer
+    dia 15 de fevereiro, no auditório do IFPI
+    Campus Teresina Central, das 8h às 12h</h4>
     <hr/>
   </div>
 )
--- src/pages/presentations.js
+++ src/pages/presentations.js
@@ -4,15 +4,17 @@ import dataJson from "../json/data"
 const Presentations = () => (
   <div>
     <h1><a name="presentations"> Palestras </a></h1>
-    {
-      dataJson.presentations.map((presentation, index) => (
-        <div className="card" key={ index }>
-          <h4 className="overlay">{ presentation.title }</h4>
-          <img src={ presentation.image } className="img-shrunked"/>
-          <h3>{ presentation.speaker }</h3>
-        </div>
-      ))
-    }
+    <div className="presentations">
+      {
+        dataJson.presentations.map((presentation, index) => (
+          <div className="card" key={ index }>
+            <h4 className="overlay">{ presentation.title }</h4>
+            <img src={ presentation.image } className="img-shrunked"/>
+            <h3>{ presentation.speaker }</h3>
+          </div>
+        ))
+      }
+    </div>
   </div>
 )
 
