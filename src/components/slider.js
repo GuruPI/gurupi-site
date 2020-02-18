0b652f0a97408f7ef5738f1a64e5d5815892a750
Renata Marques <renata.marques@codeminer42.com>
2020-02-18 12:56:29 -0300

    photo from gurupi#2

--- src/components/slider.js
+++ src/components/slider.js
@@ -2,8 +2,9 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import firstMeeting from "../images/first-meeting.jpeg"
+import gurupi1 from "../images/first-meeting.jpeg"
 import peopleFirstMeeting from "../images/people-first-meeting.jpg"
+import gurupi2 from "../images/second-meeting.jpeg"
 
 const SimpleSlider = () => {
     const settings = {
@@ -19,12 +20,16 @@ const SimpleSlider = () => {
       <h1><a name="photos">Fotos</a></h1>
         <Slider {...settings}>
           <div>
-            <img src={ firstMeeting } />
+            <img src={ gurupi1 } />
             <p>Palestrantes do Encontro GuruPI #1 que ocorreu dia 11 de janeiro de 2020</p>
           </div>
           <div>
             <img src={ peopleFirstMeeting } />
             <p>Público do encontro GuruPI #1 que ocorreu dia 11 de janeiro de 2020</p>
+          </div>
+          <div>
+            <img src={ gurupi2 } />
+            <p>Palestrantes do encontro GuruPI #2 que ocorreu dia 15 de fevereiro de 2020</p>
           </div>
         </Slider>
       </div>
